620293df3db01e603a3e00c7fbd4e2b73386ec14
Laveena Jethani <laveena.j.21@gmail.com>
2019-07-07 16:11:32 +0530

    Create next_prime.cpp

---
+++ next_prime.cpp
@@ -0,0 +1,27 @@
+# program to find next prime number
+#include<iostream>
+using namespace std;
+bool is_prime(int);
+int main()
+{
+    int n,i;
+    cout<<"enter a number";
+    cin>>n;
+    for(i=n;!is_prime(i);i++);
+    cout<<i;
+    return 0;
+}
+bool is_Prime(int a)
+{
+    int count=0;
+    for(int i=2;i<=a/2;i++)
+    {
+        if(a%i==0)
+            count++;
+        if(count==0)
+            return true;
+        else
+            return false;
+    }
+}
+
